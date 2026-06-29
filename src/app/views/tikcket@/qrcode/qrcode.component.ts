b74c1cc4ca7a4d2c985623c9f67f5f43f044ce18
AN-Fahmy <fahmyan@taibaconnect.com>
2026-06-29 15:08:51 +0300

    change qrcode page

--- src/app/views/tikcket@/qrcode/qrcode.component.ts
+++ src/app/views/tikcket@/qrcode/qrcode.component.ts
@@ -4,21 +4,35 @@ import { QRCodeComponent } from 'angularx-qrcode';
 import { BookingService } from '@core/services/booking/booking.service';
 import { Observable } from 'rxjs';
 import { NgxSpinnerService } from 'ngx-spinner';
+import { DatePipe, NgClass } from '@angular/common';
+import { EventService } from '@core/services/event/event.service';
+import { UsersService } from '@core/services/users/users.service';
 
 @Component({
   selector: 'app-qrcode',
-  imports: [QRCodeComponent],
+  imports: [QRCodeComponent, NgClass, DatePipe],
   templateUrl: './qrcode.component.html',
   styleUrl: './qrcode.component.scss'
 })
 export class QrcodeComponent implements OnInit {
   private readonly _ActivatedRoute = inject(ActivatedRoute);
   private readonly _BookingService = inject(BookingService);
+  private readonly _EventService = inject(EventService);
   private readonly _NgxSpinnerService = inject(NgxSpinnerService)
+  private readonly _UsersService = inject(UsersService)
 
   qrValue: string | null = null;
   booking$!: Observable<any>;
   qrStatus$!: Observable<boolean>;
+  eventData:any = {}
+
+  book!:any
+  bookMsg:string = ''
+  qr!:any
+  qrMsg:string = ''
+  isUsed!:boolean | string
+  qrNotFound:string = ''
+  ownerData:any = {}
 
   ngOnInit(): void {
     this._ActivatedRoute.paramMap.subscribe({
@@ -32,12 +46,22 @@ export class QrcodeComponent implements OnInit {
     });
   }
 
-  book!:any
-  bookMsg:string = ''
-  qr!:any
-  qrMsg:string = ''
-  isUsed!:boolean | string
-  qrNotFound:string = ''
+  getEventById(id:string):void{
+    this._EventService.getEventById(id).subscribe({
+      next:(res)=>{
+        this.eventData = res
+        this.getOwnerById(this.eventData?.OwnerId)
+      }
+    })
+  }
+
+  getOwnerById(id:string):void{
+    this._UsersService.getUserById(id).subscribe({
+      next:(res)=>{
+        this.ownerData = res
+      }
+    })
+  }
 
   initQrStream(qrId: string) {
     this._NgxSpinnerService.show()
@@ -45,6 +69,8 @@ export class QrcodeComponent implements OnInit {
       next: (res) => {
         this._NgxSpinnerService.hide()
         this.book = res[0]; // لأنك غالبًا راجع booking واحد
+
+        this.getEventById(this.book.EventId)
 
         if (!this.book) {
           return;
